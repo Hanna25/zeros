3f4c21e28ec3275b570351b7663024d321f55f46
Anna <a.sobolenko1194@gmail.com>
2019-10-08 00:55:43 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let factorials = {
+    single: [],
+    double: []
+  };
+  let stringFactorials = expression.split("*");
+
+  function countAllNumberFactorial(numeral, number) {
+    let quantityNumber = Math.floor(numeral / number);
+    if (Math.floor(quantityNumber / number) >= 1) {
+      quantityNumber += countAllNumberFactorial(quantityNumber, number);
+    }
+    return quantityNumber;
+  }
+  
+  function countAllNumberOddFactorial(numeral, number) {    
+    let quantityNumber = numeral / number;  
+    if (quantityNumber / number >= 1) {     
+      quantityNumber += countAllNumberOddFactorial(numeral, number * number) * 2;
+    }    
+    return Math.round(quantityNumber / 2);
+  }
+
+  
+  for (f in stringFactorials) {
+    if (stringFactorials[f].indexOf("!!") >= 0 ) {
+      factorials.double.push(stringFactorials[f].replace("!!", ""));
+    } else {
+      factorials.single.push(stringFactorials[f].replace("!", "")) 
+    }
+  }
+
+  let quantityTwo = 0, quantityFive = 0, quantityTen = 0;
+
+  for (f in factorials.single) {
+    quantityFive += countAllNumberFactorial(factorials.single[f], 5);
+    quantityTwo += countAllNumberFactorial(factorials.single[f], 2);
+  }
+
+  for (f in factorials.double) {
+    if (factorials.double[f] % 2 === 0) {
+      quantityTwo += countAllNumberFactorial(factorials.double[f], 2);
+      let currentQuantityTen = countAllNumberFactorial(factorials.double[f], 10);
+      quantityTen += currentQuantityTen; 
+      quantityFive += Math.round(currentQuantityTen / 10);
+    } else {
+      quantityFive += countAllNumberOddFactorial(factorials.double[f], 5);
+    } 
+  }
+  res = quantityFive + quantityTen;
+  if (res > quantityTwo) {
+    res = quantityTwo;
+  }
+  return res;
 }
